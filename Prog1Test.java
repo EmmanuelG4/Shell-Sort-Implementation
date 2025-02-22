23935c07d62f07dbba200de97af5bc0ac326ac8f
EmmanuelG4 <167138747+EmmanuelG4@users.noreply.github.com>
2025-02-21 20:54:14 -0600

    Updates Prog1Test.java
    Updates the test file to check all 4 codes and generate 100 arrays of size 1000.

--- Prog1Test.java
+++ Prog1Test.java
@@ -4,45 +4,46 @@ public class Prog1Test {
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-	   	MyObj a = new MyObj(1, 2, 3);
-	   	MyObj b = new MyObj(2, 2, 3);
-	   	
-		System.out.println(a.lessThan(b));
-		System.out.println(b.lessThan(a));
-		System.out.print("A: "); // added
-		a.print();
-		System.out.print("B: "); // added
-		b.print();
-		System.out.println(); // added line
 		
-		System.out.println("original array of 10:");
-		MyObj c[] = new MyObj[10]; // used to have 3
-		for (int i = 0; i < 10; i++) { // used to have 3
-			c[i] = new MyObj();
-			c[i].print();
-		}
-		System.out.println(); // added line
-	
-		System.out.println("swapped array of 10:");
-		MyObj temp = c[0];
-		c[0] = c[1];
-		c[1] = temp;
-		System.out.println(" ");
-		for (int i = 0; i < 10; i++) { // used to have 3
-			c[i].print();
-		}
-		System.out.println(" ");
-		System.out.println(); // added line
-		
-		System.out.println("sorted array:");
-		ShellSort.ShellSort(c, 3); // used to be 1
-		for (int i = 0; i < 10; i++) { // used to have 3
-			c[i].print();
-		}
-		System.out.println();
-		System.out.println("Count: " + c[0].getCount()); // not sure about this
-
+        MyObj c[] = {};
+        int code[] = {0, 1, 2, 3};
+        int hlist[] = {};
+        for(int b = 0; b < code.length; b++) {
+        	long listOfCompares[] = new long[100];
+        	long compares = 0;
+            double avgCompares = 0.0, sum = 0.0, strdDev = 0.0;
+	        for(int i = 0; i < 100; i++) {
+				c = new MyObj[1000]; 
+				for (int j = 0; j < 1000; j++) { 
+					c[j] = new MyObj();
+				}
+				hlist = ShellSort.ShellSort(c, code[b]);
+				listOfCompares[i] = c[0].getCount();
+				compares += c[0].getCount();
+				c[0].reset();
+	        }
+			
+			// Printing the hlist
+			System.out.println("Code: " + code[b]);
+			System.out.print("Hlist: ");
+			for(int i = 0; i < hlist.length; i++) {
+				System.out.print(hlist[i] + " ");
+			}
+			System.out.println();
+			
+			avgCompares = compares / 100.0;
+			System.out.printf("Average Number of Comparisons for case %d: %.2f%n", 
+					(code[b] + 1), avgCompares);
+			
+			for(int i = 0; i < listOfCompares.length; i++) {
+				sum += Math.pow(listOfCompares[i] - avgCompares, 2);
+			}
+			strdDev = Math.sqrt(sum/listOfCompares.length);
+			System.out.printf("Standard Deviation of Comparisons for case %d: %.2f%n", 
+					(code[b] + 1), strdDev);
+			System.out.println();
+        }
     }
 }
+
 
